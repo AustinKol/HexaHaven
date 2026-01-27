0aad2faa98ae9106303f91d209aa73ecb595384c
Vapidx4 <51037467+Vapidx4@users.noreply.github.com>
2026-01-27 10:59:25 -0500

    Added test routes

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -17,16 +17,37 @@ export class MainMenu extends Scene
 
         this.logo = this.add.image(512, 300, 'logo');
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
+        this.title = this.add.text(512, 400, 'Main Menu', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+        // this.input.once('pointerdown', () => {
 
+        //     this.scene.start('Game');
+
+        // });
+
+        // Button to nav to Game scene
+        const gameButton = this.add.text(512, 500, 'Start Game', {
+            fontFamily: 'Arial', fontSize: 24, color: '#00ff00',
+            backgroundColor: '#000000'
+        }).setOrigin(0.5).setPadding(10).setInteractive();
+
+        gameButton.on('pointerdown', () => {
             this.scene.start('Game');
+        });
 
+        // Button to nav to Map Gen test scene
+        const mapGenButton = this.add.text(512, 550, 'Go to Map Gen Test', {
+            fontFamily: 'Arial', fontSize: 24, color: '#00ff00',
+            backgroundColor: '#000000'
+        }).setOrigin(0.5).setPadding(10).setInteractive();
+
+        
+        mapGenButton.on('pointerdown', () => {
+            this.scene.start('MapGenTest');
         });
     }
 }
